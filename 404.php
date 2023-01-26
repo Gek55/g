7011f95bd09f05175288646dc2be13dbd8d23f08
Gek55 <111584898+Gek55@users.noreply.github.com>
2023-01-27 00:52:50 +0300

    Add files via upload

---
+++ 404.php
@@ -0,0 +1,65 @@
+<!doctype html>
+<html lang="en-US">
+<head>
+<title>Razrabotchik</title>
+<meta charset="utf-8">
+<meta name="robots" content="noindex, nofollow">
+<?php include 'lib/module/sys-meta.php';?>
+<?php include 'lib/module/sys-css.php';?>
+<?php include 'lib/module/sys-js.php';?>
+</head>
+<body style="background: #000;">
+<?php include 'lib/module/sys-php.php';?>
+<div class="page-con-content">
+	<div class="banner-con-backdrop darkmode-header" style="position:fixed;">
+		<div id="object-particles">
+		</div>
+		<div class="wavebar-con-container">
+			<div class="wavebar-con-wrap">
+			  <div class="wavebar-svg-object"></div>
+			  <div class="wavebar-svg-object"></div>
+			</div>
+		</div>
+		<div class="error-con-content">
+			
+			<div class='error-tx1-content pulsate'>
+				<h1>Варначёв Илья 9 "Б"</h1>
+			</div>
+			<div class='error-tx2-conten'>
+			<a href="http://site.ru">
+			    <p>
+					 
+				</p>
+            </a>
+			<div class="discord-tx2-mantra darkmode-txt">
+								<p>
+								<div class="video-tx1-descriptio darkmode-txt scale-content-txt-4" style="position: absolute; top: 50%; margin-top: -160px;">
+								    <h2>Паспорт проекта</h2>
+                                </div>
+                <div class="discord-tx2-mantr">
+                    <p>
+Название проекта: «Создание тематического веб-сайта»
+Руководитель проекта: Виктория Андреевна Селезнёва
+<p>Автор проекта: Варначёв Илья Максимович</p>
+Учебная дисциплина: Информатика
+<p>Тип проекта: Информационный</p>
+Цель работы: Узнать как создать собственный сайт:
+<p>1) узнать, что такое сайт</p>
+<p>2) узнать, из чего состоит сайт</p>
+<p>3) узнать, какие виды сайтов существуют</p>
+<p>4) узнать, какие языки используются для создания сайтов</p>
+<p>5) узнать, как сайты размещаются в интернете</p>
+<p>6) создать сайт и выложить его в интернет</p>
+Вопросы проекта:
+Что такое сайт, и как его сделать?
+<p>Краткое содержание проекта: в ходе проекта разобраться, как работают и пишутся сайты</p>
+<p>Результат проекта (продукт): веб-сайта</p>
+
+					</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+</body>
+</html>
---
+++ lib/module/inc-video-carousel.php
@@ -0,0 +1,92 @@
+<div class="container fill">
+	<div id="myCarousel" class="carousel slide">
+		<div class="carousel-inner">
+			<!-- Carousel Slide -->
+			<div class="item active">
+			<div class="landing-img-hero2" style="background: url(img/graphics/landing/Web-development-service-2.png)center center/703px no-repeat;">
+			</div>
+				<div class="fill">
+					<div class="video-con-container">
+						<div class='video-con-left'>
+							<div class='video-con-wrapper'>
+								<div class="video-btn-play">
+								</div>
+								<div class="video-ico-service">
+								</div>
+								<div class='video-con-animate page-video-1'>
+								</div>
+								<div class='video-img-thumbnail' style="background: url('/img/videos/1.jpg') no-repeat center; background-size: cover;">
+								</div>
+							</div>
+						</div>
+						<div class='video-con-right'>
+							<div class="video-tx1-description darkmode-txt scale-content-txt-4">
+								<div class='video-emp-block'>
+								</div>
+								<h2>Что такое сайт<br><br></h2>
+								<h3>Структура сайта<br><br></h3>
+								<h3>Виды сайтов<br><br></h3>
+								<h3>Популярные языки для создания сайтов<br><br><h3>
+                                <h3>Размещение сайта в интернете<br><br></h3>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- Carousel Slide -->
+			<div class="item">
+				<div class="fill">
+					<div class="video-con-container">
+						<div class='video-con-left'>
+							<div class='video-con-wrapper'>
+								<div class="video-btn-play">
+								</div>
+								<div class="video-ico-service">
+								</div>
+								<div class='video-con-animate page-video-2'>
+								</div>
+								<div class='video-img-thumbnail' style="background: url('/img/videos/2.jpg') no-repeat center; background-size: cover;">
+								</div>
+							</div>
+						</div>
+						<div class='video-con-right'>
+							<div class="video-tx1-description darkmode-txt scale-content-txt-5">
+								<div class='video-emp-block'>
+								</div>
+								<h2>PlayStation 3 XMB Is Now Bootable</h2>
+								<h3>For the first time ever, RPCS3 now capable of booting into VSH, also known as the VirtualShell. This handles the XrossMediaBar that most PlayStation 3 players are familar with. The XMB is used to launch games, browse media, manage settings and much more.</h3>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- Carousel Slide -->
+			<div class="item">
+				<div class="fill">
+					<div class="video-con-container">
+						<div class='video-con-left'>
+							<div class='video-con-wrapper'>
+								<div class="video-btn-play">
+								</div>
+								<div class="video-ico-service">
+								</div>
+								<div class='video-con-animate page-video-3'>
+								</div>
+								<div class='video-img-thumbnail' style="background: url('/img/videos/3.jpg') no-repeat center; background-size: cover;">
+								</div>
+							</div>
+						</div>
+						<div class='video-con-right'>
+							<div class="video-tx1-description darkmode-txt scale-content-txt-6">
+								<div class='video-emp-block'>
+								</div>
+								<h2>Improved Graphical Accuracy</h2>
+								<h3>Recently, lead graphics developer kd-11 improved RPCS3's immediate mode rendering and linear cubemap decoding which fixed broken lighting in many Insomniac games and fixed dynamic shadows in Naughty Dog games along with a handful of other titles.</h3>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
